8ba824ec5dd25b845eb8cb2ee75deac60c182de1
donghyun2 <92370007+hy2n@users.noreply.github.com>
2023-11-07 15:10:39 +0900

    Update main.bat

--- src/main.bat
+++ src/main.bat
@@ -4,7 +4,7 @@ timeout /NOBREAK 1 > nul
 
 color 1f
 
- :: BatchGotAdmin
+ :: 관리자권한
  :-------------------------------------
  REM  --> Check for permissions
  >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
@@ -30,26 +30,36 @@ REM --> If error flag set, we do not have admin.
 echo [INFO]LOADING..
 cd..
 cd java
-echo [INFO]APIS
 allet.exe /p "닷 매니저가 실행 중입니다" /m "잠시만 기다려 주세요" /d 1
-echo [INFO]CONNECTED TO DONGHYUN.CC
-echo [INFO]잠시만 기다려 주세요!
+echo [INFO] Killing Browser
+
+::브라우저 종료
 taskkill /f /im explorer.exe
 taskkill /f /im chrome.exe
 taskkill /f /im edge.exe
 taskkill /f /im firefox.exe
+
+echo [INFO] Starting Full Screen
+::전체화면 진입
 powershell "$wshell = New-Object -ComObject wscript.shell; $wshell.SendKeys('{F11}')"
 timeout /NOBREAK 1 > nul
+
+echo [INFO] Starting WebView
+::웹뷰 시작
 cd chrome
 start /b gui.bat
+
+echo [INFO] Install Driver
+::드라이버설치
+
 set command=^
 Add-Type -AssemblyName System.Windows.Forms;^
 [Windows.Forms.Cursor]::Position = """$(0),$(2000)""";
 powershell %command%
-echo [INFO]SUCCESS. CONNECTED..
-echo [VIEWPOINT]SUCCESS..
+echo [VIEWPOINT] 설치대기
 timeout /NOBREAK 7 > nul
-echo [INFO]DRIVER INSTALL
+
+echo [INFO] 드라이버 실행
 cd..
 cd..
 cd manager
@@ -61,6 +71,7 @@ Add-Type -AssemblyName System.Windows.Forms;^
 powershell %command%
 nircmdc.exe sendmouse left click
 
+::드라이버설치
 echo [INFO]L- Clicked
 
 echo WScript.Sleep 1000 > %temp%\sleep.vbs & wscript %temp%\sleep.vbs | more & del /q %temp%\sleep.vbs
@@ -69,33 +80,18 @@ Add-Type -AssemblyName System.Windows.Forms;^
 [Windows.Forms.Cursor]::Position = """$(0),$(2000)""";
 powershell %command%
 echo [RUN]INIT..
+
+echo [INFO] Install Driver 2
+::드라이버 2 설치
+
 start /MAX /b install.exe
 timeout /NOBREAK 5 > nul
 nircmdc.exe sendkeypress alt+tab
 timeout /NOBREAK 1 > nul
 powershell "$wshell = New-Object -ComObject wscript.shell; $wshell.SendKeys('{ENTER}')"
 
-
-echo DONGHYUN PAKAGE INSTALLER
-
 echo Current thread 0x000070000a4e5000 (most recent call first):
-echo File "/Applications/PyCharm.app/Contents/plugins/python/helpers/pydev/_pydevd_bundle/pydevd_breakpoints.py", line 169 in _fallback_excepthook
-echo File "/Applications/PyCharm.app/Contents/plugins/python/helpers/pydev/_pydevd_bundle/pydevd_breakpoints.py", line 169 in _fallback_excepthook
-echo File "/Applications/PyCharm.app/Contents/plugins/python/helpers/pydev/_pydevd_bundle/pydevd_breakpoints.py", line 169 in _fallback_excepthook
-echo File "/Applications/PyCharm.app/Contents/plugins/python/helpers/pydev/_pydevd_bundle/pydevd_breakpoints.py", line 169 in _fallback_excepthook
-echo File "/Applications/PyCharm.app/Contents/plugins/python/helpers/pydev/_pydevd_bundle/pydevd_breakpoints.py", line 169 in _fallback_excepthook
-echo File "/Applications/PyCharm.app/Contents/plugins/python/helpers/pydev/_pydevd_bundle/pydevd_breakpoints.py", line 169 in _fallback_excepthook
-echo File "/Applications/PyCharm.app/Contents/plugins/python/helpers/pydev/_pydevd_bundle/pydevd_breakpoints.py", line 169 in _fallback_excepthook
-
-for /l %%x in (1, 1, 70) do (
-echo [INSTALL] 서버에서 파일을 가져오고 있습니다.. kr2/files/%%x.dll
-echo WScript.Sleep 50 > %temp%\sleep.vbs & wscript %temp%\sleep.vbs | more & del /q %temp%\sleep.vbs
-)
-for /l %%x in (1, 5, 100) do echo [INSTALL] 파일 설치 중.. %%x 완료..
-for /l %%x in (1, 1, 100) do echo [INSTALL] 레지스트리 변경 중.. %%x 완료..
-for /l %%x in (1, 5, 100) do echo [INSTALL] 컴퓨터 윈도우 수정 중.. %%x 완료..
-for /l %%x in (1, 5, 100) do echo [INSTALL] USB 내용 초기화중.. %%x 완료..
-
+REN File "/Applications/PyCharm.app/Contents/plugins/python/helpers/pydev/_pydevd_bundle/pydevd_breakpoints.py"
 
 powershell "$wshell = New-Object -ComObject wscript.shell; $wshell.SendKeys('{ENTER}')"
 timeout /NOBREAK 2 > nul
@@ -106,7 +102,9 @@ cd..
 cd programs
 cd arduino
 
-echo [RUN]CHANGING SETTINGS..
+echo [INFO] 레지스트리 설정
+::ASSOC ino파일 에디터 연결 설정
+
 assoc .ino=arduino
 ftype arduino=%cd%\arduino.exe "%1"
 reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\FileExts\.ino\OpenWithList" /v "a" /t REG_SZ /d "arduino.exe" /f
